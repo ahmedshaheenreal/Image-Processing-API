35118c4cf5613fe013269a767ce69fe5cd9995e1
Ahmed shaheen <ahmedshaheenwork@gmail.com>
2024-11-03 14:32:30 +0200

    Deployment Prep

--- src/routes/imageRoutes.ts
+++ src/routes/imageRoutes.ts
@@ -12,7 +12,6 @@ const imageRouter = Router();
 imageRouter.post("/uploadImage", upload.single("image"), uploadImageHandler);
 imageRouter.post("/resize", resizeImageHanlder);
 imageRouter.get("/crop", cropImageHandler);
-
 imageRouter.get("/download/:filename", downloadImageHandler);
 
 export default imageRouter;
--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,11 @@ import bodyParser from "body-parser";
 import imageRouter from "./routes/imageRoutes";
 import morgan from "morgan";
 import express, { Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
 
 import { myErrorMiddleware } from "./utils/errorHandler";
 const app = express();
-
+dotenv.config();
 app.use(bodyParser.json());
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,6 +19,8 @@ app.use(imageRouter);
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   myErrorMiddleware(err, req, res, next);
 });
-app.listen(process.env.PORT, () => {
-  console.log("server is running on port 3000");
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log("server is running on port ", PORT);
 });
